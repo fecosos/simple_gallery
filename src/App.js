954208c21210e57574c1a85d24928e7e972711dd
Ruy Mallo <ruymallo@gmail.com>
2019-05-07 23:33:25 -0300

    Basic shape

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import image from "./assets/01.jpg";
+import arrow_right from "./assets/arrow-right.png";
+import arrow_left from "./assets/arrow-left.png";
+
+function Gallery() {
+  return (
+    <div className="Gallery">
+      <button className="button left">
+        <img src={arrow_left} className="arrow left" alt="arrow left" />
+      </button>
+      <img src={image} className="gallery-image" alt="logo" />
+      <button className="button right">
+        <img src={arrow_right} className="arrow right" alt="arrow right" />
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> ,3
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header">página web en construcción</header>
+      <Gallery />
+      <section className="footer">
+        <span className="text">encontranos en </span>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          className="instagram-link a-link"
+          href="//instagram.com"
           rel="noopener noreferrer"
+          target="_blank"
         >
-          Learn React
+          instagram
         </a>
-      </header>
+        <span className="slash">—</span>
+        <span className="email-link a-link">info.ripani@gmail.com</span>
+      </section>
     </div>
   );
 }
